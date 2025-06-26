26c64f725d9c1be9142b82c9f80ad8bd76258f9f
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-06-26 17:24:06 -0300

    Add files via upload

---
+++ CircodeAPPs1.1.0/frmsplash.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CircodeAPPs
+{
+    partial class frmsplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Consolas", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(264, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(265, 41);
+            label1.TabIndex = 0;
+            label1.Text = "CIRCO DE APPS";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.gatoemo;
+            pictureBox1.Location = new Point(206, 49);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(403, 366);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Consolas", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(270, 418);
+            label2.Name = "label2";
+            label2.Size = new Size(259, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Produced by Bigsoft";
+            // 
+            // frmsplash
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
+            BackgroundImage = Properties.Resources.OIP;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmsplash";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmsplash";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+    }
+}
