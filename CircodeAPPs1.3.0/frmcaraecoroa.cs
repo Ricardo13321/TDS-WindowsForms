c1c28348ddc2a86e583f7136fc951e783a51a54b
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-06-27 16:41:13 -0300

    Add files via upload

---
+++ CircodeAPPs1.3.0/frmcaraecoroa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircodeAPPs
+{
+    public partial class frmcaraecoroa : Form
+    {
+        int contadorCara = 0;
+        int contadorCoroa = 0;
+        Random random = new Random();
+        public frmcaraecoroa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            int resultado = random.Next(0, 2);
+            if (resultado == 0)
+            {
+                lblResultado.Text = "CARA";
+                pbxMoeda.Image = Properties.Resources.cara;
+                contadorCara++;
+            }
+            else
+            {
+                lblResultado.Text = "COROA";
+                pbxMoeda.Image = Properties.Resources.coroa;
+                contadorCoroa = contadorCara*9999999;
+            }
+            AtualizarPlacar();
+        }
+
+        private void AtualizarPlacar()
+        {
+            lblPlacar.Text = $"Placar\nCara: {contadorCara}\nCoroa: {contadorCoroa}";
+        }
+    }
+}
