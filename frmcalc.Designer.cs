f80d195b929f7bd51ea682788881de185409935e
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-07-21 17:10:48 -0300

    Add files via upload

---
+++ frmcalc.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace CircodeAPPs
+{
+    partial class frmcalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.edwin_payne_silly;
+            pictureBox1.Location = new Point(514, 184);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(290, 266);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(23, 184);
+            button2.Name = "button2";
+            button2.Size = new Size(85, 40);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(114, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 40);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(205, 184);
+            button3.Name = "button3";
+            button3.Size = new Size(85, 40);
+            button3.TabIndex = 6;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(205, 230);
+            button4.Name = "button4";
+            button4.Size = new Size(85, 40);
+            button4.TabIndex = 9;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(114, 230);
+            button5.Name = "button5";
+            button5.Size = new Size(85, 40);
+            button5.TabIndex = 8;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(23, 230);
+            button6.Name = "button6";
+            button6.Size = new Size(85, 40);
+            button6.TabIndex = 7;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(205, 276);
+            button7.Name = "button7";
+            button7.Size = new Size(85, 40);
+            button7.TabIndex = 12;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(114, 276);
+            button8.Name = "button8";
+            button8.Size = new Size(85, 40);
+            button8.TabIndex = 11;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(23, 276);
+            button9.Name = "button9";
+            button9.Size = new Size(85, 40);
+            button9.TabIndex = 10;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(306, 276);
+            button10.Name = "button10";
+            button10.Size = new Size(49, 40);
+            button10.TabIndex = 15;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(306, 230);
+            button11.Name = "button11";
+            button11.Size = new Size(49, 40);
+            button11.TabIndex = 14;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(306, 184);
+            button12.Name = "button12";
+            button12.Size = new Size(49, 40);
+            button12.TabIndex = 13;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(306, 322);
+            button13.Name = "button13";
+            button13.Size = new Size(49, 40);
+            button13.TabIndex = 16;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(205, 322);
+            button14.Name = "button14";
+            button14.Size = new Size(85, 40);
+            button14.TabIndex = 19;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(114, 322);
+            button15.Name = "button15";
+            button15.Size = new Size(85, 40);
+            button15.TabIndex = 18;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(23, 322);
+            button16.Name = "button16";
+            button16.Size = new Size(85, 40);
+            button16.TabIndex = 17;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // frmcalc
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button13);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Name = "frmcalc";
+            Text = "frmcal";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+    }
+}
---
+++ frmcalculoviagem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircodeAPPs
+{
+    public partial class frmcalculoviagem : Form
+    {
+        public frmcalculoviagem()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float alcool = 4.27f;
+            float gascomum = 6.28f;
+            float gasaditi = 6.50f;
+            float diesel = 5.93f;
+            Int32 distancia = Convert.ToInt32(txtDistancia.Text.Trim());
+            Int32 autonomia = Convert.ToInt32(txtAutonomia.Text.Trim());
+            float tempo = distancia / 90;
+            float litros = distancia / autonomia;
+            float gasta = 0;
+            if (cdbTipo.Text == "ÁLCOOL")
+            {
+                gasta = litros * alcool;
+            }
+            else if (cdbTipo.Text == "GASOLINA COMUM")
+            {
+                gasta = litros * gascomum;
+            }
+            else if (cdbTipo.Text == "GASOLINA ADITIVADA")
+            {
+                gasta = litros * gasaditi;
+            }
+            else if (cdbTipo.Text == "DIESEL")
+            {
+                gasta = litros * diesel;
+            }
+            lblLitrosGasto.Text = $"Litros gastos: {litros.ToString("F")}";
+            lblGastoTotal.Text = $"Total: {gasta.ToString("C")}";
+            lblTempo.Text = $"Tempo gasto previsto: {tempo.ToString("F")}";
+        }
+    }
+}
