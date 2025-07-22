ea4e0266497ad734fdedafe9becefe98c834e50c
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-07-22 16:02:46 -0300

    Add files via upload

--- frmcalc.Designer.cs
+++ frmcalc.Designer.cs
@@ -28,10 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
             button2 = new Button();
-            label1 = new Label();
-            label2 = new Label();
+            lblConta = new Label();
+            lblResultado = new Label();
             button1 = new Button();
             button3 = new Button();
             button4 = new Button();
@@ -40,200 +39,282 @@
             button7 = new Button();
             button8 = new Button();
             button9 = new Button();
-            button10 = new Button();
-            button11 = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button14 = new Button();
-            button15 = new Button();
+            btnSubtracao = new Button();
+            btnMultiplicacao = new Button();
+            btnDivisao = new Button();
+            btnSoma = new Button();
+            btnIgual = new Button();
+            btnVirgula = new Button();
             button16 = new Button();
+            btnFechar = new Button();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.edwin_payne_silly;
-            pictureBox1.Location = new Point(514, 184);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(290, 266);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
             // button2
             // 
-            button2.Location = new Point(23, 184);
+            button2.BackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(12, 112);
             button2.Name = "button2";
-            button2.Size = new Size(85, 40);
+            button2.Size = new Size(100, 50);
             button2.TabIndex = 2;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            button2.Text = "7";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
-            // label1
+            // lblConta
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 26);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 3;
-            label1.Text = "label1";
+            lblConta.AutoSize = true;
+            lblConta.BackColor = Color.Transparent;
+            lblConta.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            lblConta.Location = new Point(12, 9);
+            lblConta.Name = "lblConta";
+            lblConta.Size = new Size(0, 28);
+            lblConta.TabIndex = 3;
             // 
-            // label2
+            // lblResultado
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 72);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 4;
-            label2.Text = "label2";
+            lblResultado.AutoSize = true;
+            lblResultado.BackColor = Color.Transparent;
+            lblResultado.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            lblResultado.Location = new Point(12, 42);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 28);
+            lblResultado.TabIndex = 4;
             // 
             // button1
             // 
-            button1.Location = new Point(114, 184);
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(118, 112);
             button1.Name = "button1";
-            button1.Size = new Size(85, 40);
+            button1.Size = new Size(100, 50);
             button1.TabIndex = 5;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "8";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(205, 184);
+            button3.BackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(226, 112);
             button3.Name = "button3";
-            button3.Size = new Size(85, 40);
+            button3.Size = new Size(100, 50);
             button3.TabIndex = 6;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button2_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(205, 230);
+            button4.BackColor = Color.Transparent;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(226, 168);
             button4.Name = "button4";
-            button4.Size = new Size(85, 40);
+            button4.Size = new Size(100, 50);
             button4.TabIndex = 9;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            button4.Text = "6";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button2_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(114, 230);
+            button5.BackColor = Color.Transparent;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(118, 168);
             button5.Name = "button5";
-            button5.Size = new Size(85, 40);
+            button5.Size = new Size(100, 50);
             button5.TabIndex = 8;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button2_Click;
             // 
             // button6
             // 
-            button6.Location = new Point(23, 230);
+            button6.BackColor = Color.Transparent;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(12, 168);
             button6.Name = "button6";
-            button6.Size = new Size(85, 40);
+            button6.Size = new Size(100, 50);
             button6.TabIndex = 7;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button2_Click;
             // 
             // button7
             // 
-            button7.Location = new Point(205, 276);
+            button7.BackColor = Color.Transparent;
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(226, 224);
             button7.Name = "button7";
-            button7.Size = new Size(85, 40);
+            button7.Size = new Size(100, 50);
             button7.TabIndex = 12;
-            button7.Text = "button7";
-            button7.UseVisualStyleBackColor = true;
+            button7.Text = "3";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button2_Click;
             // 
             // button8
             // 
-            button8.Location = new Point(114, 276);
+            button8.BackColor = Color.Transparent;
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(118, 224);
             button8.Name = "button8";
-            button8.Size = new Size(85, 40);
+            button8.Size = new Size(100, 50);
             button8.TabIndex = 11;
-            button8.Text = "button8";
-            button8.UseVisualStyleBackColor = true;
+            button8.Text = "2";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button2_Click;
             // 
             // button9
             // 
-            button9.Location = new Point(23, 276);
+            button9.BackColor = Color.Transparent;
+            button9.FlatStyle = FlatStyle.Popup;
+            button9.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(12, 224);
             button9.Name = "button9";
-            button9.Size = new Size(85, 40);
+            button9.Size = new Size(100, 50);
             button9.TabIndex = 10;
-            button9.Text = "button9";
-            button9.UseVisualStyleBackColor = true;
+            button9.Text = "1";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button2_Click;
             // 
-            // button10
+            // btnSubtracao
             // 
-            button10.Location = new Point(306, 276);
-            button10.Name = "button10";
-            button10.Size = new Size(49, 40);
-            button10.TabIndex = 15;
-            button10.Text = "button10";
-            button10.UseVisualStyleBackColor = true;
+            btnSubtracao.BackColor = Color.Transparent;
+            btnSubtracao.FlatStyle = FlatStyle.Popup;
+            btnSubtracao.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSubtracao.Location = new Point(332, 224);
+            btnSubtracao.Name = "btnSubtracao";
+            btnSubtracao.Size = new Size(50, 50);
+            btnSubtracao.TabIndex = 15;
+            btnSubtracao.Text = "-";
+            btnSubtracao.UseVisualStyleBackColor = false;
             // 
-            // button11
+            // btnMultiplicacao
             // 
-            button11.Location = new Point(306, 230);
-            button11.Name = "button11";
-            button11.Size = new Size(49, 40);
-            button11.TabIndex = 14;
-            button11.Text = "button11";
-            button11.UseVisualStyleBackColor = true;
+            btnMultiplicacao.BackColor = Color.Transparent;
+            btnMultiplicacao.FlatStyle = FlatStyle.Popup;
+            btnMultiplicacao.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMultiplicacao.Location = new Point(332, 168);
+            btnMultiplicacao.Name = "btnMultiplicacao";
+            btnMultiplicacao.Size = new Size(50, 50);
+            btnMultiplicacao.TabIndex = 14;
+            btnMultiplicacao.Text = "x";
+            btnMultiplicacao.UseVisualStyleBackColor = false;
             // 
-            // button12
+            // btnDivisao
             // 
-            button12.Location = new Point(306, 184);
-            button12.Name = "button12";
-            button12.Size = new Size(49, 40);
-            button12.TabIndex = 13;
-            button12.Text = "button12";
-            button12.UseVisualStyleBackColor = true;
+            btnDivisao.BackColor = Color.Transparent;
+            btnDivisao.FlatStyle = FlatStyle.Popup;
+            btnDivisao.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDivisao.Location = new Point(332, 112);
+            btnDivisao.Name = "btnDivisao";
+            btnDivisao.Size = new Size(50, 50);
+            btnDivisao.TabIndex = 13;
+            btnDivisao.Text = "/";
+            btnDivisao.UseVisualStyleBackColor = false;
             // 
-            // button13
+            // btnSoma
             // 
-            button13.Location = new Point(306, 322);
-            button13.Name = "button13";
-            button13.Size = new Size(49, 40);
-            button13.TabIndex = 16;
-            button13.Text = "button13";
-            button13.UseVisualStyleBackColor = true;
+            btnSoma.BackColor = Color.Transparent;
+            btnSoma.FlatStyle = FlatStyle.Popup;
+            btnSoma.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSoma.Location = new Point(332, 280);
+            btnSoma.Name = "btnSoma";
+            btnSoma.Size = new Size(50, 50);
+            btnSoma.TabIndex = 16;
+            btnSoma.Text = "+";
+            btnSoma.UseVisualStyleBackColor = false;
+            btnSoma.Click += btnSoma_Click;
             // 
-            // button14
+            // btnIgual
             // 
-            button14.Location = new Point(205, 322);
-            button14.Name = "button14";
-            button14.Size = new Size(85, 40);
-            button14.TabIndex = 19;
-            button14.Text = "button14";
-            button14.UseVisualStyleBackColor = true;
+            btnIgual.BackColor = Color.Blue;
+            btnIgual.FlatStyle = FlatStyle.Popup;
+            btnIgual.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnIgual.Location = new Point(226, 280);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(100, 50);
+            btnIgual.TabIndex = 19;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = false;
+            btnIgual.Click += btnIgual_Click;
             // 
-            // button15
+            // btnVirgula
             // 
-            button15.Location = new Point(114, 322);
-            button15.Name = "button15";
-            button15.Size = new Size(85, 40);
-            button15.TabIndex = 18;
-            button15.Text = "button15";
-            button15.UseVisualStyleBackColor = true;
+            btnVirgula.BackColor = Color.Transparent;
+            btnVirgula.FlatStyle = FlatStyle.Popup;
+            btnVirgula.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnVirgula.Location = new Point(118, 280);
+            btnVirgula.Name = "btnVirgula";
+            btnVirgula.Size = new Size(100, 50);
+            btnVirgula.TabIndex = 18;
+            btnVirgula.Text = ",";
+            btnVirgula.UseVisualStyleBackColor = false;
+            btnVirgula.Click += btnVirgula_Click;
             // 
             // button16
             // 
-            button16.Location = new Point(23, 322);
+            button16.BackColor = Color.Transparent;
+            button16.FlatStyle = FlatStyle.Popup;
+            button16.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button16.Location = new Point(12, 280);
             button16.Name = "button16";
-            button16.Size = new Size(85, 40);
+            button16.Size = new Size(100, 50);
             button16.TabIndex = 17;
-            button16.Text = "button16";
-            button16.UseVisualStyleBackColor = true;
+            button16.Text = "0";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button2_Click;
+            // 
+            // btnFechar
+            // 
+            btnFechar.BackColor = Color.Red;
+            btnFechar.BackgroundImageLayout = ImageLayout.None;
+            btnFechar.FlatStyle = FlatStyle.Popup;
+            btnFechar.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            btnFechar.ForeColor = Color.FromArgb(192, 0, 0);
+            btnFechar.Location = new Point(276, 336);
+            btnFechar.Name = "btnFechar";
+            btnFechar.Size = new Size(106, 50);
+            btnFechar.TabIndex = 20;
+            btnFechar.Text = "Fechar";
+            btnFechar.UseVisualStyleBackColor = false;
+            btnFechar.Click += btnFechar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.rengar;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(393, 420);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 21;
+            pictureBox1.TabStop = false;
             // 
             // frmcalc
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button14);
-            Controls.Add(button15);
+            ClientSize = new Size(393, 420);
+            Controls.Add(btnFechar);
+            Controls.Add(btnIgual);
+            Controls.Add(btnVirgula);
             Controls.Add(button16);
-            Controls.Add(button13);
-            Controls.Add(button10);
-            Controls.Add(button11);
-            Controls.Add(button12);
+            Controls.Add(btnSoma);
+            Controls.Add(btnSubtracao);
+            Controls.Add(btnMultiplicacao);
+            Controls.Add(btnDivisao);
             Controls.Add(button7);
             Controls.Add(button8);
             Controls.Add(button9);
@@ -242,23 +323,23 @@
             Controls.Add(button6);
             Controls.Add(button3);
             Controls.Add(button1);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(lblResultado);
+            Controls.Add(lblConta);
             Controls.Add(button2);
             Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "frmcalc";
             Text = "frmcal";
+            Load += frmcalc_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Button button2;
-        private Label label1;
-        private Label label2;
+        private Label lblConta;
+        private Label lblResultado;
         private Button button1;
         private Button button3;
         private Button button4;
@@ -267,12 +348,14 @@
         private Button button7;
         private Button button8;
         private Button button9;
-        private Button button10;
-        private Button button11;
-        private Button button12;
-        private Button button13;
-        private Button button14;
-        private Button button15;
+        private Button btnSubtracao;
+        private Button btnMultiplicacao;
+        private Button btnDivisao;
+        private Button btnSoma;
+        private Button btnIgual;
+        private Button btnVirgula;
         private Button button16;
+        private Button btnFechar;
+        private PictureBox pictureBox1;
     }
 }
--- frmcalc.cs
+++ frmcalc.cs
@@ -12,9 +12,172 @@ namespace CircodeAPPs
 {
     public partial class frmcalc : Form
     {
+        double primeiroNumero = 0;
+        string operador = "";
+        bool novoNumero = true;
+        bool virgulaDigitada = false;
         public frmcalc()
         {
             InitializeComponent();
+        }
+
+        private void frmcalc_Load(object sender, EventArgs e)
+        {
+            lblConta.Text = "0";
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (novoNumero)
+            {
+                lblConta.Text = btn.Text;
+                novoNumero = false;
+                virgulaDigitada = false;
+            }
+            else
+            {
+                if (lblConta.Text == "0" && btn.Text == "0")
+                {
+                    return;
+                }
+                if (lblConta.Text == "0" && btn.Text != "0")
+                {
+                    lblConta.Text = btn.Text;
+                }
+                else
+                {
+                    lblConta.Text += btn.Text;
+                }
+            }
+            if (operador == "")
+            {
+                lblResultado.Text = lblConta.Text;
+            }
+            else
+            {
+                string primeiroNumStr = primeiroNumero.ToString();
+                if (primeiroNumStr.Contains("."))
+                {
+                    primeiroNumStr = primeiroNumStr.Replace(".", ",");
+                }
+                lblResultado.Text = $"{primeiroNumStr} {operador} {lblConta.Text}";
+            }
+        }
+
+        private void btnVirgula_Click(object sender, EventArgs e)
+        {
+            if (!virgulaDigitada)
+            {
+                if (lblConta.Text == "" || novoNumero)
+                {
+                    lblConta.Text = "0,";
+                    novoNumero = false;
+                }
+                else
+                {
+                    lblConta.Text += ",";
+                }
+                virgulaDigitada = true;
+            }
+            if (operador == "")
+            {
+                lblResultado.Text = lblConta.Text;
+            }
+            else
+            {
+                string primeiroNumStr = primeiroNumero.ToString();
+                if (primeiroNumStr.Contains("."))
+                {
+                    primeiroNumStr = primeiroNumStr.Replace(".", ",");
+                }
+                lblResultado.Text = $"{primeiroNumStr} {operador} {lblConta.Text}";
+            }
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (!novoNumero && operador != "")
+            {
+                CalcularResultado();
+            }
+
+            primeiroNumero = double.Parse(lblConta.Text);
+            operador = btn.Text;
+            novoNumero = true;
+            string primeiroNumeroStr = primeiroNumero.ToString();
+            if (primeiroNumeroStr.Contains(","))
+            {
+                primeiroNumeroStr = primeiroNumeroStr.Replace(".", ",");
+            }
+            lblResultado.Text = $"{primeiroNumeroStr} {operador}";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            string segundoNumeroString = lblConta.Text;
+            string operadorAnterior = operador;
+            CalcularResultado();
+            string primeiroNumeoStr = primeiroNumero.ToString();
+            if (primeiroNumeoStr.Contains(","))
+            {
+                primeiroNumeoStr = primeiroNumeoStr.Replace(".", ",");
+            }
+            lblResultado.Text = $"{primeiroNumeoStr} {operadorAnterior} {segundoNumeroString}";
+            operador = "";
+            novoNumero = true;
+            virgulaDigitada = false;
+
+        }
+
+        private void CalcularResultado()
+        {
+            if (operador == "" || novoNumero)
+            {
+                return;
+            }
+            double segundoNumero = double.Parse(lblConta.Text);
+            double resultado = 0;
+            switch (operador)
+            {
+                case "+":
+                    resultado = primeiroNumero+segundoNumero; 
+                    break;
+                case "-":
+                    resultado = primeiroNumero-segundoNumero;
+                    break;
+                case "X":
+                    resultado = primeiroNumero * segundoNumero;
+                    break;
+                case "/":
+                    if (segundoNumero != 0)
+                    {
+                        resultado = primeiroNumero / segundoNumero;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não é possível realizar uma divisão por ero", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        lblConta.Text = "0";
+                        primeiroNumero = 0;
+                        operador = "";
+                        novoNumero = true;
+                        virgulaDigitada = false;
+                        lblResultado.Text = "0";
+                        return;
+                    }
+                    break;
+                default:
+                    resultado = 0;
+                    break;
+            }
+            lblConta.Text = resultado.ToString();
+            primeiroNumero = resultado;
         }
     }
 }
